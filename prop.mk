17f84248db73623d640781b999c6b9c77962f145
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-05-25 13:21:09 +0530

    msm8953-common: Drop common default dpi move dpi to device tree cuz rosy has 320 dpi
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- prop.mk
+++ prop.mk
@@ -106,8 +106,7 @@ ro.qualcomm.cabl=0 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
-vendor.gralloc.enable_fb_ubwc=1 \
-ro.sf.lcd_density=420
+vendor.gralloc.enable_fb_ubwc=1
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
